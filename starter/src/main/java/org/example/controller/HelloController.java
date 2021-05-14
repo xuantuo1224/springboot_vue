3a6b1015c1f2b7983fd097e6ef69e8f1e53dc853
xuantuo <yunzheng.yyz@alibaba-inc.com>
2021-05-14 23:55:00 +0800

    + vue 资源

--- starter/src/main/java/org/example/controller/HelloController.java
+++ starter/src/main/java/org/example/controller/HelloController.java
@@ -1,14 +1,21 @@
 package org.example.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 public class HelloController {
 
     @RequestMapping("/")
     public String index() {
         return "Hello!";
+    }
+
+    @RequestMapping("/homepage")
+    public String homePage() {
+        return  "index";
     }
 
 }
